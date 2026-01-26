13a723b508dab325820ed1ef1aecc204711e4df9
harikarthick12 <harikarthicksrini@gmail.com>
2026-01-26 21:20:27 +0530

    Fix: Configure strict CORS for frontend URL

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // Enable CORS for client
+  app.enableCors({
+    origin: ['https://learnedge-client.onrender.com', 'http://localhost:3000'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   const port = process.env.PORT || 3001;
   await app.listen(port);
   console.log(`Application is running on: http://localhost:${port}`);
